02b306fbf66bd6e7eb0321388e71dc43d0ef0a46
parkyuhyeon <pyh1111802@naver.com>
2024-06-15 15:54:50 +0900

    test

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,12 +1,29 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool _isChecked = false;
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text("hello world"),
+    return Scaffold(
+      body: Center(
+        child: CupertinoSwitch(
+          value: _isChecked,
+          onChanged: (value) => setState(
+            () {
+              _isChecked = value;
+            },
+          ),
+        ),
+      ),
     );
   }
 }
