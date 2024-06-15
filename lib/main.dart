b2281cd688250164e55e64c8d7aafd5342499f65
parkyuhyeon <pyh1111802@naver.com>
2024-06-15 15:47:21 +0900

    ✅ :: test

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gsm_inside_flutter/views/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +10,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return const MaterialApp(
+      home: HomePage(),
+    );
   }
 }
